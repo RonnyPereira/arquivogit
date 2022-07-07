9a0032f65dd6c157ca0e1c53b0f20a626333ae6e
ronnyps@hotmail.com <ronnyps@hotmail.com>
2022-07-07 14:57:56 -0400

    07.01

---
+++ JavaScript: tipos, variáveis e funções/conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dado
+// booleanos
+
+
+
+// cpversão implicita
+
+const numero = 456;
+const numeroString = "456"
+
+console.log(numero+numeroString);
+
+
+//cpmversão explicita
---
+++ JavaScript: tipos, variáveis e funções/type-string.js
@@ -0,0 +1,16 @@
+const texto1 ="Olá, mundo";
+
+const texto2 =" Olá, mundo!";
+
+const senha = "senhaSuperSegura456!";
+
+const stringDeNumeoros ="34567";
+
+const citacao = "Meu nome e: ";
+
+const meuNome = "Ronny";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
