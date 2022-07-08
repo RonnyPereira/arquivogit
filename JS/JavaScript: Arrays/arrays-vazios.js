cf73bec9da48396e42225ed84390597a92cf473d
ronnyps@hotmail.com <ronnyps@hotmail.com>
2022-07-08 14:48:54 -0400

    08.02

---
+++ JS/JavaScript: Arrays/arrays-vazios.js
@@ -0,0 +1,20 @@
+// const arrayVazia = [];
+
+// console.log(arrayVazia)
+// console.log(arrayVazia.length)
+
+// const arrayvazio = [];
+// console.log(arrayVazia[0])
+
+// const arrayvazia = [,,,];
+// console.log(arrayvazia.length)
+// console.log(arrayvazia[0])
+// console.log(arrayvazia[1])
+// console.log(arrayvazia[2])
+
+
+const arrayVazia = [,,,];
+console.log(arrayVazia.length)
+arrayVazia.push(50)
+console.log(arrayVazia)
+console.log(arrayVazia.length)
