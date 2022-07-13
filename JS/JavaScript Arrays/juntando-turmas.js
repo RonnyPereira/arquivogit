52933c424e23720615aa19910e18c095a06b1681
Ronny Pereira <ronnyps@hotmail.com>
2022-07-12 20:05:23 -0400

    12.04

---
+++ JS/JavaScript Arrays/juntando-turmas.js
@@ -0,0 +1,8 @@
+
+const salaDePython = ['Melissa','Helena','Rodrigo']
+
+const salaDeJavaScript = ['Ju', 'Leo','Ronny']
+
+const salaUnificada = salaDePython.concat(salaDeJavaScript)
+
+console.log(salaUnificada)
