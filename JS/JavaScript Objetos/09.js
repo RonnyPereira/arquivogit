23a4af3389cc4af38c9af8c6aee95839c404e70e
ronnyps@hotmail.com <ronnyps@hotmail.com>
2022-07-15 13:54:10 -0400

    15.03

--- JS/JavaScript Objetos/09.js
+++
@@ -1,2 +0,0 @@
-//Adicionar uma propriedade que permita "acoes", para que os clientes que estao cadastrados
-// consigam fazer operacoes bancarias
---
+++ JS/JavaScript Objetos/09cliente.js
@@ -0,0 +1,23 @@
+//Adicionar uma propriedade que permita "acoes", para que os clientes que estao cadastrados
+// consigam fazer operacoes bancarias
+
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "5465465464",
+    email: "andre@gmail.com",
+    fones: ["6546466546", "89789797"],
+    
+    dependentes:[ {
+        nome:"Sara",
+        parentesco: "filha",
+        dataNasc: "20/03/2011"
+            },      
+
+        {
+        nome:"Samia Maria",
+        parentesco:"filha",
+        dataNasc:"04/01/2014"
+        }
+    ],
+}
