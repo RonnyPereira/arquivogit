3ee7f9259b1ae6702e52212e5b0214024862bc12
ronnyps@hotmail.com <ronnyps@hotmail.com>
2022-07-22 16:39:42 -0400

    22.02

--- JS/JavaScript I algoritmos de ordenação/05OrdemaldeValores.js
+++ JS/JavaScript I algoritmos de ordenação/05OrdemaldeValores.js
@@ -5,6 +5,7 @@ function insertionSort(lista){
     for(let atual =0; atual < lista.length; atual++){
 
         let analise = atual;
+        
         while (analise>0 && lista[analise].preco<lista[analise -1].preco){
 
             let itemAnalise = livros[analise];
